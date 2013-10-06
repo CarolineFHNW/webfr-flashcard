bb89af8ea58107ac982a6c4a5370829c2fafef11
CarolineFHNW <caroline.schnell@students.fhnw.ch>
2013-10-06 12:24:30 +0200

    Arbeitsblatt 4, Aufgabe 2 - Ergänzung zu IndexController

--- src/main/java/ch/fhnw/edu/flashcard/web/IndexController.java
+++ src/main/java/ch/fhnw/edu/flashcard/web/IndexController.java
@@ -16,7 +16,7 @@ public class IndexController {
 	@RequestMapping(method = RequestMethod.GET)		// Bei GET-Requests soll diese Methode ohne Parameter aufgerufen werden
 
 	public String list(Model uiModel) { 			// Methode bekommt leeres Model und füllt dieses mit den Questionnaires (Businesslogik)
-
+													// Dabei ist questionnaires der Key und die Questionnaires der Value
 		uiModel.addAttribute("questionnaires", Questionnaire.findAllQuestionnaires());
 		log.debug("index called");					// Ausgabe auf der Konsole zur Kontrolle, ob index aufgerufen wurde
 		return "index";								// Der Name der view
