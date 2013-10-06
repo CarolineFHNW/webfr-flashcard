cb06f24d6a1ca219de56837cdd675a1004c3d07b
CarolineFHNW <caroline.schnell@students.fhnw.ch>
2013-10-06 12:03:06 +0200

    Arbeitsblatt 4, Aufgabe 2 - IndexController erstellen

---
+++ src/main/java/ch/fhnw/edu/flashcard/web/IndexController.java
@@ -0,0 +1,25 @@
+package ch.fhnw.edu.flashcard.web;
+
+import org.apache.log4j.Logger;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import ch.fhnw.edu.flashcard.domain.Questionnaire;
+
+@Controller											// Die Klasse als PageController deklarieren
+@RequestMapping("/")								// Reagiert auf eine URL mit .../flashcard (also index)
+
+public class IndexController {
+	private Logger log = Logger.getLogger(this.getClass());
+	@RequestMapping(method = RequestMethod.GET)		// Bei GET-Requests soll diese Methode ohne Parameter aufgerufen werden
+
+	public String list(Model uiModel) { 			// Methode bekommt leeres Model und füllt dieses mit den Questionnaires (Businesslogik)
+
+		uiModel.addAttribute("questionnaires", Questionnaire.findAllQuestionnaires());
+		log.debug("index called");					// Ausgabe auf der Konsole zur Kontrolle, ob index aufgerufen wurde
+		return "index";								// Der Name der view
+	}
+
+}
